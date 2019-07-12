f2d71fcbf12ddc31203b6600c7c76cf0c5586a91
Tom Dunlap <tomdunlap@Toms-MacBook-Pro.local>
2019-07-12 00:07:31 -0700

    feat: added enum to traffic light code.

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -23,7 +23,7 @@ void MessageQueue<T>::send(T &&msg)
 
 /* Implementation of class "TrafficLight" */
 
-/* 
+ 
 TrafficLight::TrafficLight()
 {
     _currentPhase = TrafficLightPhase::red;
@@ -44,6 +44,7 @@ TrafficLightPhase TrafficLight::getCurrentPhase()
 void TrafficLight::simulate()
 {
     // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
+    threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this));
 }
 
 // virtual function which is executed in a thread
@@ -53,6 +54,21 @@ void TrafficLight::cycleThroughPhases()
     // and toggles the current phase of the traffic light between red and green and sends an update method 
     // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
     // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
+    while (true)
+    {
+        if (_currentPhase == TrafficLightPhase::red)
+        {
+            _currentPhase = TrafficLightPhase::green;
+        }
+        else
+        {
+            _currentPhase = TrafficLightPhase::red;
+        }
+        //_queue->send(std::move(_currentPhase));
+        int cycle = 2 * ((double)std::rand() / (double)RAND_MAX) + 4;
+        std::this_thread::sleep_for(std::chrono::seconds(cycle));
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
 }
 
-*/
+
